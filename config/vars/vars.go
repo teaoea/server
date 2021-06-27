bb3768a2d5f64b14e87af78c078d5d48bf655ffb
YaoHui <teaoea@gmail.com>
2021-06-27 20:04:20 +0800

    chore: logs are stored by date

--- config/vars/vars.go
+++ config/vars/vars.go
@@ -1,23 +1,66 @@
 package vars
 
-import "server/config"
+import (
+	"fmt"
+	"time"
+
+	"server/config"
+)
 
 var (
 	conf config.Config
 	c    = conf.Yaml()
 
-	DB0 = config.PostgresqlClient(c.Postgresql.User[0], c.Postgresql.Password[0], c.Postgresql.Host[0], c.Postgresql.Port[0], c.Postgresql.Name[0])
+	DB0 = config.PostgresqlClient(
+		c.Postgresql.User[0], c.Postgresql.Password[0], c.Postgresql.Host[0],
+		c.Postgresql.Port[0], c.Postgresql.Name[0],
+	)
 
-	RedisCode   = config.RedisClient(c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 0)
-	RedisToken  = config.RedisClient(c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 1)
-	RedisLogoff = config.RedisClient(c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 2)
+	RedisCode = config.RedisClient(
+		c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 0,
+	)
 
-	MongoSuffix       = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "conf", "suffixes")
-	MongoIpaddr       = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "conf", "ipaddr")
-	MongoHttp         = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "log", "http")
-	MongoError        = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "log", "error")
-	MongoAngularError = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "angular", "error")
-	MongoQuery        = config.MongoClient(c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0], "conf", "query")
+	RedisToken = config.RedisClient(
+		c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 1,
+	)
+
+	RedisLogoff = config.RedisClient(
+		c.Redis.Host[0], c.Redis.Port[0], c.Redis.Password[0], 2,
+	)
+
+	MongoSuffix = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0],
+		c.Mongo.Port[0], "conf", "suffixes",
+	)
+
+	MongoIpaddr = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0],
+		c.Mongo.Port[0], "conf", "ipaddr",
+	)
+
+	MongoQuery = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0],
+		c.Mongo.Port[0], "conf", "query",
+	)
+	MongoHttp = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0], c.Mongo.Port[0],
+		fmt.Sprintf(
+			"log-%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day(),
+		),
+		fmt.Sprintf(
+			"%d-%d", time.Now().Hour(), time.Now().Minute(),
+		),
+	)
+
+	MongoError = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0],
+		c.Mongo.Port[0], "log", "error",
+	)
+
+	MongoAngularError = config.MongoClient(
+		c.Mongo.User[0], c.Mongo.Password[0], c.Mongo.Host[0],
+		c.Mongo.Port[0], "angular", "error",
+	)
 
 	KeyToken = c.Key.Token
 
