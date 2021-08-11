00f27701a917b5c29982253546f81bdf3cda9b02
YaoHui <teaoea@gmail.com>
2021-08-11 21:27:44 +0800

    chore: Phone number prefix

--- models/user.go
+++ models/user.go
@@ -1,19 +1,20 @@
 package models
 
 type User struct {
-	Id           int64  `json:"id"`
-	Username     string `json:"username"`
-	Password     string `json:"password"`
-	Email        string `json:"email" `
-	EmailActive  bool   `json:"email_active" `
-	Number       string `json:"number"`
-	NumberActive bool   `json:"number_active" `
-	Avatar       string `json:"avatar" `
-	Gender       string `json:"gender" `
-	CreatedAt    string `json:"created_at" `
-	IsActive     bool   `json:"is_active" `
-	IsAdmin      bool   `json:"is_admin" `
-	IsHide       bool   `json:"is_hide"`
+	Id          int64  `json:"id"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email" `
+	EmailActive bool   `json:"email_active" `
+	Prefix      string `json:"prefix"`
+	Phone       string `json:"phone"`
+	PhoneActive bool   `json:"phone_active" `
+	Avatar      string `json:"avatar" `
+	Gender      string `json:"gender" `
+	CreatedAt   string `json:"created_at" `
+	IsActive    bool   `json:"is_active" `
+	IsAdmin     bool   `json:"is_admin" `
+	IsHide      bool   `json:"is_hide"`
 }
 
 func (User) TableName() string {
